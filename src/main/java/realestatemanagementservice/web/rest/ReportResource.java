99df93475859c117d9c072db6c97180a07284b32
Thomas Parcher <s-thomas.parcher@lwtech.edu>
2020-05-17 16:20:48 -0700

    Refactored ReportResource a little

--- src/main/java/realestatemanagementservice/web/rest/ReportResource.java
+++ src/main/java/realestatemanagementservice/web/rest/ReportResource.java
@@ -11,10 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import io.github.jhipster.service.filter.LocalDateFilter;
-import realestatemanagementservice.service.RentQueryService;
-import realestatemanagementservice.service.RentService;
-import realestatemanagementservice.service.dto.RentCriteria;
-import realestatemanagementservice.service.dto.RentDTO;
+import io.github.jhipster.service.filter.LongFilter;
+import realestatemanagementservice.service.*;
+import realestatemanagementservice.service.dto.*;
 
 @RestController
 @RequestMapping("/api")
@@ -25,8 +24,7 @@ public class ReportResource {
 	private String applicationName;
 	
 	private final RentService rentService;
-
-    private final RentQueryService rentQueryService;
+	private final RentQueryService rentQueryService;
 	
 	public ReportResource(RentService rentService, RentQueryService rentQueryService) {
 		this.rentService = rentService;
@@ -34,12 +32,12 @@ public class ReportResource {
 	}
 	
 	/**
-     * {@code GET  /rents/paid} : get all the rents paid through the specified date for that month.
+     * {@code GET  /reports/paid} : get all the rents paid through the specified date for that month.
      *
      * @param date the criteria which the requested entities should match.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of rents paid in body.
      */
-    @GetMapping("/rents/paid")
+    @GetMapping("/reports/paid")
     public ResponseEntity<List<RentDTO>> getRentsPaid(@RequestParam("date") 
     		@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         log.debug("REST request to get Rents paid for date criteria: {}", date);
@@ -57,5 +55,5 @@ public class ReportResource {
         List<RentDTO> rents = rentQueryService.findByCriteria(criteria);
         return ResponseEntity.ok().body(rents);
     }
-
+    
 }
