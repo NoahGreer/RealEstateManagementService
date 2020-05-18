76e73887936f5150add3f4f4c42a3436e8f0ec6d
Andrew Franko <andrewhfranko@gmail.com>
2020-05-18 11:51:28 -0700

    EB to EBResource, ALResource not working weirdly

--- src/main/java/realestatemanagementservice/web/rest/ActiveLeaseResource.java
+++ src/main/java/realestatemanagementservice/web/rest/ActiveLeaseResource.java
@@ -19,30 +19,30 @@ import realestatemanagementservice.service.dto.LeaseDTO;
 @RestController
 @RequestMapping("/api")
 public class ActiveLeaseResource {
-	private final Logger log = LoggerFactory.getLogger(ActiveLeaseResource.class);
+	private final static Logger log = LoggerFactory.getLogger(ActiveLeaseResource.class);
 	
-	LocalDate today = LocalDate.now();
+	static LocalDate today = LocalDate.now();
 	
 	@Value("${jhipster.clientApp.name}")
 	private String applicationName;
 	
-	private LeaseService leaseService;
+	private final LeaseService leaseService;
 
-    private LeaseQueryService leaseQueryService;
+    private final LeaseQueryService leaseQueryService;
 	
-	public void ActvieLeaseResource(LeaseService leaseService, LeaseQueryService leaseQueryService) {
+	public ActiveLeaseResource(LeaseService leaseService, LeaseQueryService leaseQueryService) {
 		this.leaseService = leaseService;
 		this.leaseQueryService = leaseQueryService;
 	}
 	
 	/**
-     * {@code GET  /rents/paid} : get all the rents paid through the specified date for that month.
+     * {@code GET  /leases/active} : get all the active leases, ie today is between date signed and move out date.
      *
      * @param date the criteria which the requested entities should match.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of rents paid in body.
      */
-    @GetMapping("/rents/paid")
-    public ResponseEntity<List<LeaseDTO>> getActiveLeases() {
+    @GetMapping("/leases/active")
+    public ResponseEntity<List<LeaseDTO>> getActiveLeasesDTO() {
         log.debug("REST request to get active Leases");
         
         LocalDateFilter datesignedFilter = new LocalDateFilter();
@@ -58,6 +58,35 @@ public class ActiveLeaseResource {
         List<LeaseDTO> activeLeases = leaseQueryService.findByCriteria(criteria);
         return ResponseEntity.ok().body(activeLeases);
     }
+    /**
+     * {@code GET  /leases/activeID} : get all the active lease IDs, ie today is between date signed and move out date.
+     *
+     * @param date the criteria which the requested entities should match.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of rents paid in body.
+     */
+    @GetMapping("/leases/activeID")
+    @SuppressWarnings("null")
+	public static ResponseEntity<List<Long>> getActiveLeasesID() {
+        log.debug("REST request to get only the active Lease IDs");
+        
+        LocalDateFilter datesignedFilter = new LocalDateFilter();
+        datesignedFilter.setGreaterThanOrEqual(today);
+        
+        LocalDateFilter endDateFilter = new LocalDateFilter();
+        endDateFilter.setLessThan(today);
+        
+        LeaseCriteria criteria = new LeaseCriteria();
+        criteria.setDateSigned(datesignedFilter);
+        criteria.setEndDate(endDateFilter);
+        
+        List<LeaseDTO> activeLeasesDTO = leaseQueryService.findByCriteria(criteria);
 
+        List<Long> activeLeases = null;
+        for(LeaseDTO lease : activeLeasesDTO) {
+        	activeLeases.add(lease.getId());
+        }
+        return ResponseEntity.ok().body(activeLeases);
+    }
+    
 }
 
