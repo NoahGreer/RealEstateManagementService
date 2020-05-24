ed4b16fd93659ee18e4a1d52c934a596d3d4e77d
Noah Greer <noah.greer@gmail.com>
2020-05-23 17:10:35 -0700

    Fix indentation in ReportResourceIT

--- src/test/java/realestatemanagementservice/web/rest/ReportResourceIT.java
+++ src/test/java/realestatemanagementservice/web/rest/ReportResourceIT.java
@@ -38,89 +38,89 @@ import realestatemanagementservice.repository.RentRepository;
 @AutoConfigureMockMvc
 @WithMockUser
 public class ReportResourceIT {
-	
+
 	@Autowired
-    private RentRepository rentRepository;
+	private RentRepository rentRepository;
 
-    @Autowired
-    private EntityManager em;
+	@Autowired
+	private EntityManager em;
 
-    @Autowired
-    private MockMvc restRentMockMvc;
+	@Autowired
+	private MockMvc restRentMockMvc;
 
 	@Test
-    @Transactional
-    public void getRentsPaid() throws Exception {
-    	
-    	// Dates around our criteria date included and excluded by the query criteria
-    	LocalDate criteriaDate = LocalDate.of(2020, 5, 10);
-    	LocalDate firstDayOfCriteriaDateMonth = criteriaDate.withDayOfMonth(1);
-    	LocalDate firstDayOfPriorMonth = firstDayOfCriteriaDateMonth.minusMonths(1);
-    	LocalDate firstDayOfFollowingMonth = firstDayOfCriteriaDateMonth.plusMonths(1);    	
-    	
-    	// This Rent paid from last month should not appear in the results
-        Rent rentDueAndPaidPriorMonth = new Rent()
-                .dueDate(firstDayOfPriorMonth)
-                .recievedDate(firstDayOfPriorMonth)
-                .amount(new BigDecimal("800.01"));
-        
-    	// This Rent that has not been paid from last month should not appear in the results
-        Rent rentDueAndNotPaidPriorMonth = new Rent()
-                .dueDate(firstDayOfPriorMonth)
-                .recievedDate(null)
-                .amount(new BigDecimal("825.03"));
+	@Transactional
+	public void getRentsPaid() throws Exception {
 
-        // This Rent that has not been paid from the criteria month should not appear in the results
-        Rent rentDueCriteriaMonthUnpaid = new Rent()
-                .dueDate(firstDayOfCriteriaDateMonth)
-                .recievedDate(null)
-                .amount(new BigDecimal("1065.05"));
-        
-        // This Rent paid on time from the criteria month should appear in the results
-        Rent rentDueCriteriaMonthOnTime = new Rent()
-                .dueDate(firstDayOfCriteriaDateMonth)
-                .recievedDate(firstDayOfCriteriaDateMonth)
-                .amount(new BigDecimal("975.00"));
-        
-        // This Rent paid within the grace period from the criteria month should appear in the results
-        Rent rentDueCriteriaMonthWithinGracePeriod = new Rent()
-                .dueDate(firstDayOfCriteriaDateMonth)
-                .recievedDate(firstDayOfCriteriaDateMonth.plusDays(5))
-                .amount(new BigDecimal("985.00"));
-        
-        // This Rent that has not been paid from the criteria month should not appear in the results
-        Rent rentDueFollowingMonthUnpaid = new Rent()
-                .dueDate(firstDayOfFollowingMonth)
-                .recievedDate(null)
-                .amount(new BigDecimal("1165.07"));
+		// Dates around our criteria date included and excluded by the query criteria
+		LocalDate criteriaDate = LocalDate.of(2020, 5, 10);
+		LocalDate firstDayOfCriteriaDateMonth = criteriaDate.withDayOfMonth(1);
+		LocalDate firstDayOfPriorMonth = firstDayOfCriteriaDateMonth.minusMonths(1);
+		LocalDate firstDayOfFollowingMonth = firstDayOfCriteriaDateMonth.plusMonths(1);
 
+		// This Rent paid from last month should not appear in the results
+		Rent rentDueAndPaidPriorMonth = new Rent()
+				.dueDate(firstDayOfPriorMonth)
+				.recievedDate(firstDayOfPriorMonth)
+				.amount(new BigDecimal("800.01"));
 
-        Set<Rent> entities = new HashSet<>();
-        entities.add(rentDueAndPaidPriorMonth);
-        entities.add(rentDueAndNotPaidPriorMonth);
-        entities.add(rentDueCriteriaMonthUnpaid);
-        entities.add(rentDueCriteriaMonthOnTime);
-        entities.add(rentDueCriteriaMonthWithinGracePeriod);
-        entities.add(rentDueFollowingMonthUnpaid);
-    	
-        // Initialize the database
-        rentRepository.saveAll(entities);
-        rentRepository.flush();
+		// This Rent that has not been paid from last month should not appear in the
+		// results
+		Rent rentDueAndNotPaidPriorMonth = new Rent()
+				.dueDate(firstDayOfPriorMonth)
+				.recievedDate(null)
+				.amount(new BigDecimal("825.03"));
 
-        // Get list of rents paid
-        restRentMockMvc.perform(get("/api/reports/rents/paid?date=" + criteriaDate.format(DateTimeFormatter.ISO_LOCAL_DATE)))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(jsonPath("$", hasSize(2)))
-            .andExpect(jsonPath("$[0].id", equalTo(rentDueCriteriaMonthOnTime.getId().intValue())))
-            .andExpect(jsonPath("$[0].dueDate", is("2020-05-01")))
-            .andExpect(jsonPath("$[0].recievedDate", is("2020-05-01")))
-            .andExpect(jsonPath("$[0].amount", is(975.00)))
-            .andExpect(jsonPath("$[0].leaseId", nullValue()))
-            .andExpect(jsonPath("$[1].id", equalTo(rentDueCriteriaMonthWithinGracePeriod.getId().intValue())))
-            .andExpect(jsonPath("$[1].dueDate", is("2020-05-01")))
-            .andExpect(jsonPath("$[1].recievedDate", is("2020-05-06")))
-            .andExpect(jsonPath("$[1].amount", is(985.00)))
-            .andExpect(jsonPath("$[1].leaseId", nullValue()));
-     }
+		// This Rent that has not been paid from the criteria month should not appear in
+		// the results
+		Rent rentDueCriteriaMonthUnpaid = new Rent()
+				.dueDate(firstDayOfCriteriaDateMonth)
+				.recievedDate(null)
+				.amount(new BigDecimal("1065.05"));
+
+		// This Rent paid on time from the criteria month should appear in the results
+		Rent rentDueCriteriaMonthOnTime = new Rent()
+				.dueDate(firstDayOfCriteriaDateMonth)
+				.recievedDate(firstDayOfCriteriaDateMonth)
+				.amount(new BigDecimal("975.00"));
+
+		// This Rent paid within the grace period from the criteria month should appear
+		// in the results
+		Rent rentDueCriteriaMonthWithinGracePeriod = new Rent()
+				.dueDate(firstDayOfCriteriaDateMonth)
+				.recievedDate(firstDayOfCriteriaDateMonth.plusDays(5))
+				.amount(new BigDecimal("985.00"));
+
+		// This Rent that has not been paid from the criteria month should not appear in
+		// the results
+		Rent rentDueFollowingMonthUnpaid = new Rent()
+				.dueDate(firstDayOfFollowingMonth)
+				.recievedDate(null)
+				.amount(new BigDecimal("1165.07"));
+
+		Set<Rent> entities = new HashSet<>();
+		entities.add(rentDueAndPaidPriorMonth);
+		entities.add(rentDueAndNotPaidPriorMonth);
+		entities.add(rentDueCriteriaMonthUnpaid);
+		entities.add(rentDueCriteriaMonthOnTime);
+		entities.add(rentDueCriteriaMonthWithinGracePeriod);
+		entities.add(rentDueFollowingMonthUnpaid);
+
+		// Initialize the database
+		rentRepository.saveAll(entities);
+		rentRepository.flush();
+
+		// Get list of rents paid
+		restRentMockMvc.perform(get("/api/reports/rents/paid?date=" + criteriaDate.format(DateTimeFormatter.ISO_LOCAL_DATE)))
+				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(jsonPath("$", hasSize(2)))
+				.andExpect(jsonPath("$[0].id", equalTo(rentDueCriteriaMonthOnTime.getId().intValue())))
+				.andExpect(jsonPath("$[0].dueDate", is("2020-05-01")))
+				.andExpect(jsonPath("$[0].recievedDate", is("2020-05-01")))
+				.andExpect(jsonPath("$[0].amount", is(975.00))).andExpect(jsonPath("$[0].leaseId", nullValue()))
+				.andExpect(jsonPath("$[1].id", equalTo(rentDueCriteriaMonthWithinGracePeriod.getId().intValue())))
+				.andExpect(jsonPath("$[1].dueDate", is("2020-05-01")))
+				.andExpect(jsonPath("$[1].recievedDate", is("2020-05-06")))
+				.andExpect(jsonPath("$[1].amount", is(985.00))).andExpect(jsonPath("$[1].leaseId", nullValue()));
+	}
 }
